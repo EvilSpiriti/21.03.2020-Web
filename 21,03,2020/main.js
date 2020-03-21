261fcb81da99401ccd4ea5b3b9f295b434df762f
EvilSpiriti <59861314+EvilSpiriti@users.noreply.github.com>
2020-03-21 04:40:51 +0300

    Special For LDPK

---
+++ 21,03,2020/main.js
@@ -0,0 +1,4 @@
+butt.onclick = function() {
+    var val = document.getElementById("name").value;
+    var vol = document.getElementById("fmail").value;
+    document.getElementById("str").innerHTML=val + " " + vol;}
