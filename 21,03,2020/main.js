b4968f3993b8e73e7d91fcc37ad06468ef484283
EvilSpiriti <59861314+EvilSpiriti@users.noreply.github.com>
2020-03-21 04:42:53 +0300

    Delete main.js

--- 21,03,2020/main.js
+++
@@ -1,4 +0,0 @@
-butt.onclick = function() {
-    var val = document.getElementById("name").value;
-    var vol = document.getElementById("fmail").value;
-    document.getElementById("str").innerHTML=val + " " + vol;}
